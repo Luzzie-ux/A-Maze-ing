d67ea7f8ad1539f055a0991ba187f4f990472425
Luzzie <estelarodrigoluz@gmail.com>
2026-08-17 11:04:10 +0100

    send

--- src/mazegen/maze_generator.py
+++ src/mazegen/maze_generator.py
@@ -1,3 +1,4 @@
+import random
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -6,26 +7,35 @@ if TYPE_CHECKING:
 
 class MazeGenerator:
     def __init__(self, config: MazeConfig) -> None:
-        self.config = config
-        self.width = config.width
-        self.height = config.height
+        self.config: MazeConfig = config
+        self.width: int = config.width
+        self.height: int = config.height
+        self.center: tuple[int, int] = (
+            int(self.width / 2),
+            int(self.height / 2),
+        )
         self.grid: list[list[int]] = []
 
+        random.seed(config.seed)
         self._create_grid()
 
     def _create_grid(self) -> None:
         ex, ey = self.config.entry
         sx, sy = self.config.exit
+        cx, cy = self.center
         for i in range(self.height):
             row: list = []
             for j in range(self.width):
                 if i == ey and j == ex:
-                    row.append(0b0000)
+                    row.append("*")
                     continue
                 if i == sy and j == sx:
-                    row.append(0b0000)
+                    row.append("*")
                     continue
-                row.append(0b1111)
+                if cy in {i, j} or cx in {j, i}:
+                    row.append("#")
+                    continue
+                row.append(".")
             self.grid.append(row)
 
-    def _carve_maze(self) -> None: ...
+    def _42_pattern(self) -> None: ...
